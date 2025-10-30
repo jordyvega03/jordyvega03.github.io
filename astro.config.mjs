b601443700c06408daa230c2a31323728c2aa523
Jordy Vega <jordy.vega@softsvgroup.com>
2025-10-29 20:03:55 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://jordyvega.github.io",
+  site: "https://jordyvega03.github.io",
   outDir: "dist",
   output: "static",
 });
